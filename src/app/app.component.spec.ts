eb596436ea63ea1184f8185312d155fb1d864253
it <email@e.com>
2022-09-25 14:29:04 +0300

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-six-angular-intensive'`, () => {
+  it(`should have as title 'project_five_angular_intensive'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-six-angular-intensive');
+    expect(app.title).toEqual('project_five_angular_intensive');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-six-angular-intensive app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project_five_angular_intensive app is running!');
   });
 });
